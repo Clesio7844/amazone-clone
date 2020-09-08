c9df63eb15107825fbe985e773ba45830440a11c
david <daviddecastro42@live.co.uk>
2020-09-09 00:07:27 +0100

    build header component, had logo, search bar and basket icon and update the header css page

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD8BtzXDTmOScnWNN4wCfVBRGGIBXkToYw',
+  authDomain: 'e-clone-21f39.firebaseapp.com',
+  databaseURL: 'https://e-clone-21f39.firebaseio.com',
+  projectId: 'e-clone-21f39',
+  storageBucket: 'e-clone-21f39.appspot.com',
+  messagingSenderId: '1011055575835',
+  appId: '1:1011055575835:web:7fdccd2df54860448b89be'
+};

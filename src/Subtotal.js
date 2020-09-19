53e1f8f2fcb3e6a967f5299245ee48a445ff06a3
david <daviddecastro42@live.co.uk>
2020-09-19 21:52:15 +0100

    start checkout component page and css page

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,9 +3,11 @@ import './Subtotal.css'
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from './StateProvider';
 import { getBasketTotal } from './reducer';
+import { useHistory } from 'react-router-dom';
 
 function Subtotal() {
 
+    const history = useHistory()
     const [{ basket }, dispatch] = useStateValue()
 
     return <div className="subtotal">
@@ -27,7 +29,7 @@ function Subtotal() {
             thousandSeparator={true}
             prefix={"$"}
         />
-        <button>Pocess to Checkout</button>
+        <button onClick={e => history.push("/payment")}>Pocess to Checkout</button>
         </div>;
     
 }
